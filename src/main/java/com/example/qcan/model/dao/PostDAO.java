9cce80d54c1347f7f5da03e86ed2203455720c7c
phihung17 <hung01092003@gmail.com>
2023-12-09 17:45:44 +0700

    GetPost

--- src/main/java/com/example/qcan/model/dao/PostDAO.java
+++ src/main/java/com/example/qcan/model/dao/PostDAO.java
@@ -7,6 +7,7 @@ import com.example.qcan.model.bean.Post;
 
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class PostDAO {
     public void createPost(Post post)
@@ -118,4 +119,110 @@ public class PostDAO {
             throw new RuntimeException(e);
         }
     }
+    public ArrayList<Post> getListPostByIdUser(int IdUser)
+    {
+        ArrayList<Post> list = new ArrayList<Post>();
+
+        try {
+            Connection connection = ConnectDB.getConnection();
+            String sql = "SELECT * from post inner join account on post.IdUser = account.id where IdUser = ? order by post.PostTime DESC";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, IdUser);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Post post = new Post();
+                post.setIdPost(resultSet.getInt("IdPost"));
+                Timestamp timestamp = resultSet.getTimestamp("PostTime");
+                if (timestamp != null) {
+                    post.setPostTime(timestamp.toLocalDateTime());
+                }
+                post.setIdUser(resultSet.getInt("IdUser"));
+                post.setSongName(resultSet.getString("SongName"));
+                post.setSongType(resultSet.getString("SongType"));
+                post.setMusician(resultSet.getString("Musician"));
+                post.setPostContent(resultSet.getString("PostContent"));
+                post.setVideoUrl(resultSet.getString("Video"));
+                post.setFullname(resultSet.getString("fullname"));
+                post.setNickname(resultSet.getString("nickname"));
+                post.setAvatar(resultSet.getString("avatar"));
+                list.add(post);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+    public ArrayList<Post> getListPostByIdFler(int IdFler)
+    {
+        ArrayList<Post> list = new ArrayList<Post>();
+
+        try {
+            Connection connection = ConnectDB.getConnection();
+            String sql = "SELECT * from post inner join follow on post.IdUser = follow.idFled\n" +
+                    "inner join account on post.IdUser = account.id\n" +
+                    "where follow.idFler = ? order by post.PostTime DESC ";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1, IdFler);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Post post = new Post();
+                post.setIdPost(resultSet.getInt("IdPost"));
+                Timestamp timestamp = resultSet.getTimestamp("PostTime");
+                if (timestamp != null) {
+                    post.setPostTime(timestamp.toLocalDateTime());
+                }
+                post.setIdUser(resultSet.getInt("IdUser"));
+                post.setSongName(resultSet.getString("SongName"));
+                post.setSongType(resultSet.getString("SongType"));
+                post.setMusician(resultSet.getString("Musician"));
+                post.setPostContent(resultSet.getString("PostContent"));
+                post.setVideoUrl(resultSet.getString("Video"));
+                post.setFullname(resultSet.getString("fullname"));
+                post.setNickname(resultSet.getString("nickname"));
+                post.setAvatar(resultSet.getString("avatar"));
+                list.add(post);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+    public ArrayList<Post> getListPostByTimeDES()
+    {
+        ArrayList<Post> list = new ArrayList<Post>();
+
+        try {
+            Connection connection = ConnectDB.getConnection();
+            String sql = "SELECT * from post,account order by PostTime DESC";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Post post = new Post();
+                post.setIdPost(resultSet.getInt("IdPost"));
+                Timestamp timestamp = resultSet.getTimestamp("PostTime");
+                if (timestamp != null) {
+                    post.setPostTime(timestamp.toLocalDateTime());
+                }
+                post.setIdUser(resultSet.getInt("IdUser"));
+                post.setSongName(resultSet.getString("SongName"));
+                post.setSongType(resultSet.getString("SongType"));
+                post.setMusician(resultSet.getString("Musician"));
+                post.setPostContent(resultSet.getString("PostContent"));
+                post.setVideoUrl(resultSet.getString("Video"));
+                post.setFullname(resultSet.getString("fullname"));
+                post.setNickname(resultSet.getString("nickname"));
+                post.setAvatar(resultSet.getString("avatar"));
+                list.add(post);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
+
 }
