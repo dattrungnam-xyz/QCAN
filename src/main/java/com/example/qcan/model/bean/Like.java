a3e4fa109b3cb67f427eb2e3030658bef5d614f3
CoderVu <vunguyen.170803@gmail.com>
2023-12-09 12:02:07 +0700

    gan xong like

---
+++ src/main/java/com/example/qcan/model/bean/Like.java
@@ -0,0 +1,31 @@
+package com.example.qcan.model.bean;
+
+public class Like {
+    private int id;
+    private int postId;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
